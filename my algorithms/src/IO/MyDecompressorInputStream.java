52bae7c4a65c5b4f19bca96dc6d991b9de643be8
Michael Kratik <michael.kratik@gmail.com>
2016-04-13 08:53:15 +0300

    IO

---
+++ my algorithms/src/IO/MyDecompressorInputStream.java
@@ -0,0 +1,21 @@
+package IO;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class MyDecompressorInputStream extends InputStream {
+
+	private InputStream in;
+	
+	public MyDecompressorInputStream(InputStream in) {
+		this.in = in;
+	}
+
+	@Override
+	public int read() throws IOException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
